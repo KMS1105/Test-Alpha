08232716d810c3f4750bda5507fc8c12dcf8bff9
KMS1105 <134215862+KMS1105@users.noreply.github.com>
2023-08-02 14:52:38 +0900

    Add files via upload

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,42 +1,20 @@
 import streamlit as st
-import openai
 
 """https://www.youtube.com/watch?v=8u2PngR2xpM, Terminal=streamlit run (File Path)"""
-
-openai.api_key = "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"
 
 st.title("Test")
 
 with st.form("form"):
-    User_input = st.text_input("Prompt")
-    size = st.selectbox("Size", ["1024x1024", "512x512", "256x256"])
+    User_input = st.text_input("ID")
+    User_input2 = st.text_input("PassWord")
     submit = st.form_submit_button("Submit")
 
+
 if submit and User_input:
-    GPT_Prompt = [{
-        "role" : "system",
-        "content" : "Imagine the detail appeareance of the input., briefly"
-    }]
+    with st.spinner("Wait..."):
+        if User_input == 'abc' and User_input2 == 'abc':
+            st.write('pass')
     
-    """with st.spinner("Waiting for DallE..."):
-        DallE_response = openai.Image.create(
-            prompt=User_input,
-            size="1024x1024"
-        )
-
-    st.image(DallE_response["data"][0]["url"])"""
-
-    GPT_Prompt.append({
-        "role" : "system",
-        "content" : User_input
-    })
-    
-    with st.spinner("Waiting for ChatGPT..."):
-        GPT_response = openai.ChatCompletion.create(
-            model = "gpt-3.5-turbo",
-            messages=GPT_Prompt
-        )
-    
-    prompt = GPT_response["choices"][0]["message"]["content"]
-    st.write(prompt)
+        else:
+            st.write('ID나 PW가 일치 하지 않음')
 
