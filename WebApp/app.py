1ffb05f38afb588a191fb89880656a7fafe102e5
KMS1105 <134215862+KMS1105@users.noreply.github.com>
2023-08-02 15:20:41 +0900

    Add files via upload

--- WebApp/app.py
+++ WebApp/app.py
@@ -3,10 +3,10 @@ import streamlit as st
 """https://www.youtube.com/watch?v=8u2PngR2xpM, Terminal=streamlit run (File Path)"""
 
 tab1, tab2= st.tabs(['Tab A' , 'Tab B'])
+st.title("Test")   
+IDPW = []
 
 with tab1:
-    st.title("Test")   
-    IDPW = []
 
     with st.form("Setting"):
         User_setting = st.text_input("ID")
@@ -23,7 +23,12 @@ with tab1:
         User_input2 = st.text_input("PassWord")
         submit = st.form_submit_button("Submit")
       
-with tab2:        
+with tab2:   
+    with st.form("Setting"):
+        User_setting = st.text_input("ID")
+        User_setting2 = st.text_input("PassWord")
+        submit = st.form_submit_button("Submit")
+             
     if submit and User_input and User_input2:
         with st.spinner("Saving..."):
             if [User_input, User_input2] == IDPW[0]:
