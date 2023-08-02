8919ac191146b4dce20262c5fd99dc49f19d85cc
KMS1105 <134215862+KMS1105@users.noreply.github.com>
2023-08-02 15:34:09 +0900

    Add files via upload

--- WebApp/app.py
+++ WebApp/app.py
@@ -8,7 +8,7 @@ IDPW = []
 with tab1:
     st.title("Test") 
     
-    with st.form("MY_form"):
+    with st.form("form1"):
         User_setting = st.text_input("ID")
         User_setting2 = st.text_input("PassWord")
         submit = st.form_submit_button("Submit")
@@ -16,7 +16,8 @@ with tab1:
 
     if submit and User_setting and User_setting2:
         with st.spinner("Saving..."):
-            IDPW.append([User_setting, User_setting2])
+            IDPW.append(User_setting)
+            IDPW.append(User_setting2)
       
 with tab2:   
     st.title("Test") 
@@ -28,7 +29,7 @@ with tab2:
              
     if submit and User_input and User_input2:
         with st.spinner("Saving..."):
-            if [User_input, User_input2] == IDPW[0]:
+            if User_input == IDPW[0] and User_input2 == IDPW(1):
                 st.write('Pass')    
         
             else:
